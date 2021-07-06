6f3400ef975372b8158f808e40ac1a4c01cf7102
djsteve33 <swomble@aol.com>
2021-07-06 15:11:05 -0400

    commit tree 0befc0f576523004c405aa3959eaa7e354b776a8

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(driver, myString) {
+    return driver.filter( possibleMatch =>
+      possibleMatch.toLowerCase() === myString.toLowerCase()
+    )
+  }
+  
+  function fuzzyMatch(driver, yourString) {
+    return driver.filter( possibleMatch =>
+      possibleMatch.toLowerCase().indexOf(yourString.toLowerCase()) === 0
+    )
+  }
+  
+  function matchName(driver, ourString) {
+    return driver.filter( record => record.name === ourString)
+  }
